2fa454193f163e226c1b0f13dfec42339428813f
Terrence Ryan <terry@terrenceryan.com>
2012-03-21 17:36:40 -0400

    Starting script to generate static pages for content so google doesn't hate us.

---
+++ rhino/createStaticPages.js
@@ -0,0 +1,9 @@
+// 	Read in all files.
+// 	Filter content pages
+// 	Read in contents of template file 
+
+//	For all content pages
+// 		Insert Article Details
+//		Set page title.
+//		Create containing folder
+//		Write page to disk 
